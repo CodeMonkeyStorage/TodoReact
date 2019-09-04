db3661b6105c850bd84c48760efb3753f3a58da7
Wesley <wrudie@uci.edu>
2019-09-04 16:50:13 -0700

    Updates

---
+++ precache-manifest.b0523982928cdb7614b8df39ff017d04.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fc580e21c17c50eb4fdaa8e8affe396",
+    "url": "/TodoReact/index.html"
+  },
+  {
+    "revision": "ac8d084b548e5fce36f5",
+    "url": "/TodoReact/static/css/main.8d76a197.chunk.css"
+  },
+  {
+    "revision": "72b651ec8164a86c47f3",
+    "url": "/TodoReact/static/js/2.4738f7ab.chunk.js"
+  },
+  {
+    "revision": "ac8d084b548e5fce36f5",
+    "url": "/TodoReact/static/js/main.8830b7af.chunk.js"
+  },
+  {
+    "revision": "ca0ffb2a8ddc82591378",
+    "url": "/TodoReact/static/js/runtime~main.f64e5596.js"
+  }
+]);
